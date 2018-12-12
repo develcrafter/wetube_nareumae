92c42b5b3959148b4943d2eaf2e6482afbf2f46b
develcrafter <nareumae@gmail.com>
2018-12-12 10:08:09 +0900

    recap controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,8 +10,8 @@ const DELETE_VIDEO = "/:id/delete";
 */
 
 export const videos = (res, req) => req.send("videos");
-export const upload= (res, req) => req.send("upload");
-export const videoDetail= (res, req) => req.send("video detail");
-export const editVideo= (res, req) => req.send("edit video");
-export const deleteVideo= (res, req) => req.send("delete video");
+export const upload = (res, req) => req.send("upload");
+export const videoDetail = (res, req) => req.send("video detail");
+export const editVideo = (res, req) => req.send("edit video");
+export const deleteVideo = (res, req) => req.send("delete video");
 
