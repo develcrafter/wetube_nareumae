598e2f265188c41ce440ccd08945a2b9ed00568d
develcrafter <nareumae@gmail.com>
2018-12-17 12:42:14 +0900

    partila and local variable in pug is done

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
+import helmet from "helmet";
+import { localsMiddleware } from "./middlewares";
+
+import morgan from "morgan";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
@@ -27,12 +29,12 @@ const app = express();
 app.set("view engine", "pug");
 //middleware
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
-
+app.use(localsMiddleware);
 //router?
 
 app.use(routes.home, globalRouter);
