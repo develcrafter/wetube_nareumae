0ccfb605d4584a6a80142d97cb1c93929bac5135
develcrafter <nareumae@gmail.com>
2019-01-02 11:32:43 +0900

    search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,10 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home"}); 
-export const search = (req, res) => res.render("search", { pageTitle : "Search"}); 
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
 
 /* 
 const VIDEOS = "/videos";
@@ -9,9 +14,13 @@ const EDIT_VIDEO = "/:id/edit"; // why this route is different with user's edit?
 const DELETE_VIDEO = "/:id/delete";
 */
 
-export const videos = (res, req) => req.render("videos", { pageTitle : "Videos"});
-export const upload = (res, req) => req.render("upload", { pageTitle : "Upload"});
-export const videoDetail = (res, req) => req.render("videoDetail", { pageTitle : "Video Detail"});
-export const editVideo = (res, req) => req.render("editVideo", { pageTitle : "Edit Video"});
-export const deleteVideo = (res, req) => req.render("deleteVideo", { pageTitle : "Delete Video"});
-
+export const videos = (res, req) =>
+  req.render("videos", { pageTitle: "Videos" });
+export const upload = (res, req) =>
+  req.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (res, req) =>
+  req.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (res, req) =>
+  req.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (res, req) =>
+  req.render("deleteVideo", { pageTitle: "Delete Video" });
